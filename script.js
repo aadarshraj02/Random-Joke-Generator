765966c7ec31675b1a0859d2adb8aba4e4dd046f
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-19 21:19:55 +0530

    select and stored impotant elements in a const

---
+++ script.js
@@ -0,0 +1,5 @@
+const url = "https://official-joke-api.appspot.com/random_joke";
+
+const btn = document.querySelector("button");
+const joke = document.getElementById("jokes");
+const punch = document.getElementById("punch");
