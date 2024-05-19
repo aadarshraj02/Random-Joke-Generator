988189d2451a1722ec0512f6ef0b88ae7b340c67
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-19 21:21:05 +0530

    added event listener to button

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const url = "https://official-joke-api.appspot.com/random_joke";
 const btn = document.querySelector("button");
 const joke = document.getElementById("jokes");
 const punch = document.getElementById("punch");
+
+btn.addEventListener("click", getJokes);
